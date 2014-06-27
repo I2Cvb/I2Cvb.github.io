42af7c39fe78462386106e62d9e33c06fe9f59f2
Guillaume Lemaitre <glemaitre@visor.udg.edu>
2014-06-27 10:40:10 +0200

    Fixed some graphical layout

--- docs/examples/scrolling-nav/js/scrolling-nav.js
+++ docs/examples/scrolling-nav/js/scrolling-nav.js
@@ -3,19 +3,21 @@ $(window).scroll(function() {
     if ($(".navbar").offset().top > 50) {
 	$(".navbar-fixed-top").addClass("navbar-small");
 	$(".navbar-fixed-top").removeClass("navbar-big");
-	$(".centered").addClass("navbar-footer");
+	$(".centered").addClass("navbar-footer-text");
 	$(".navbar-brand-logo").addClass("scrolled");
 	$(".right-side-bar").addClass("scrolled");
 	$(".left-side-bar").addClass("scrolled");
-	$(".home-section").addClass("scrolled");	
+	$(".home-section").addClass("scrolled");
+	$(".navbar-footer").removeClass("collapse");
     } else {
 	$(".navbar-fixed-top").addClass("navbar-big");
 	$(".navbar-fixed-top").removeClass("navbar-small");
-	$(".centered").removeClass("navbar-footer");
+	$(".centered").removeClass("navbar-footer-text");
 	$(".navbar-brand-logo").removeClass("scrolled");
 	$(".right-side-bar").removeClass("scrolled");
 	$(".left-side-bar").removeClass("scrolled");
 	$(".home-section").removeClass("scrolled");
+	$(".navbar-footer").addClass("collapse");
     }
 });
 
@@ -29,9 +31,9 @@ $(function() {
         event.preventDefault();
     });
 });
-
-// jQuery in order to obtain autoheight for the panel
-$(function() {
+/*
+$( "#contact1" ).resize( function() {
+    // Resize the contact panels to be equal size
     var $hContact1 = $( "#contact1" ).height();
     var $hContact2 = $( "#contact2" ).height();
     if ($hContact1 > $hContact2){
@@ -41,3 +43,59 @@ $(function() {
         $( "#contact1" ).height( $hContact2 );
     }
 });
+
+$( "#contact2" ).resize( function() {
+    // Resize the contact panels to be equal size
+    var $hContact1 = $( "#contact1" ).height();
+    var $hContact2 = $( "#contact2" ).height();
+    if ($hContact1 > $hContact2){
+        $( "#contact2" ).height( $hContact1 );
+    }
+    else {
+        $( "#contact1" ).height( $hContact2 );
+    }
+});
+*/
+$(function() {
+    // Resize the contact panels to be equal size
+    var $hContact1 = $( "#contact1" ).height();
+    var $hContact2 = $( "#contact2" ).height();
+    if ($hContact1 > $hContact2){
+        $( "#contact2" ).height( $hContact1 );
+    }
+    else {
+        $( "#contact1" ).height( $hContact2 );
+    }
+});
+
+//jQuery to initialize the webpage
+$(window).ready(function() {
+    // Collapse the navbar-footer initially
+    $(".navbar-footer").addClass("collapse");
+/*
+    // Resize the contact panels to be equal size
+    var $hContact1 = $( "#contact1" ).height();
+    var $hContact2 = $( "#contact2" ).height();
+    if ($hContact1 > $hContact2){
+
+        $( "#contact2" ).height( $hContact1 );
+    }
+    else {
+        $( "#contact1" ).height( $hContact2 );
+    }
+*/
+});
+/*
+//jQuery to execute when the window is resized
+$(window).resize(function() {
+    // Resize the contact panels to be equal size
+    var $hContact1 = $( "#contact1" ).height();
+    var $hContact2 = $( "#contact2" ).height();
+    if ($hContact1 > $hContact2){
+        $( "#contact2" ).height( $hContact1 );
+    }
+    else {
+        $( "#contact1" ).height( $hContact2 );
+    }
+});
+*/
